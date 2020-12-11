9f7b0ddb9d0854b0ec897eac8377304e323af05b
BrianMbugua5693 <brianmbugua5693@gmail.com>
2020-12-11 07:48:47 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+var subtract = function(number1, number2, number3){
+    return number1 - number2 - number3;
+    };
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt( prompt("Another number:"));
+    var number3 = parseInt(prompt("Next number"));
+    alert(subtract(number1, number2, number3));
+    
