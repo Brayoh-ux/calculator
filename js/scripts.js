4d02b5a6ae473e70f9d45c8cfb957c60803edb05
BrianMbugua5693 <brianmbugua5693@gmail.com>
2020-12-18 01:55:53 +0300

    calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 
-// var subtract = function(number1, number3){
-//     return number1*number1  / number3;
-//     };
-//     var number1 = parseInt(prompt("Enter a number:"));
-//     // var number2 = parseInt( prompt("Another number:"));
-//     var number3 = parseInt(prompt("Next number"));
-//     alert(subtract(number1, number3));
-    
-    
-    var bmi = function(height, weight,constant){
-        return  weight / height/height  * constant ;
+var add = function(number1, number2){
+    return number1 + number2;
     };
-    var height = parseInt(prompt("Enter your height in cm"));
-    var weight = parseInt(prompt("Enter your weight"));
-    var constant = parseInt(10000);
-    alert(bmi(height, weight, constant).toFixed(3));
+var subtract = function(number1, number2) {
+    return number1 - number2;
+};
+ var multiply = function(number1, number2) {
+    return number1 * number2;
+ };
+var divide = function(number1, number2) {
+    return number1 / number2;
+};
+    
+$(document).ready(function(){
+    $("form#add").submit(function(event){
+        event.preventDefault();
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        var result = subtract(number1, number2);
+        $("#output").text(result);
+    });
+    
+});
